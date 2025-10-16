9765538cf55f8957c52b2bb513824e277c4b2c7a
Joao Victor Soares <joao.soares@havan.com.br>
2025-10-16 16:55:07 -0300

    mudança nas tabelas

--- Models/Tarefa.cs
+++ Models/Tarefa.cs
@@ -5,4 +5,5 @@ public class Tarefa
     public int Id { get; set; }
     public string Titulo { get; set; } = string.Empty;
     public bool Concluida { get; set; } = false;
+    public int IdLista { get; set; }
 }
